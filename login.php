ab2c86b371abaa98678144fcd20979e31e8600d6
Ivaldir Batalha <62729507+ivaldir301@users.noreply.github.com>
2020-11-29 20:31:24 +0000

    ...
    ...

---
+++ login.php
@@ -0,0 +1,66 @@
+<html>
+<body>
+
+
+<?php 
+
+// Conecção
+require_once 'db_connect.php';
+
+// Sessão
+session_start();
+
+if(isset($_POST['enviar'])):
+    $erros = array();
+    $login = mysqli_real_escape_string($connect, $_POST['login']);
+    $senha = mysqli_real_escape_string($connect, $_POST['senha']);
+
+    if(empty($login) or empty($senha)):
+        $erros[] = "<li>O campo login e o campo senha não podem ficar vazios.</li>";
+    else:
+        $sql = "SELECT login FROM usuarios WHERE login = '$login'";
+        $resultado = mysqli_query($connect, $sql);
+
+        if(mysqli_num_rows($resultado) > 0):
+            $sql = "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'";
+            $resultado = mysqli_query($connect, $sql);
+            
+            if(mysqli_num_rows($resultado) ==  1):
+               //echo "Login efectuado.<br>";  
+               $dados = mysqli_fetch_array($resultado);
+               $_SESSION['logado'] = true;
+               $_SESSION['id_usuario'] = dados['id'];
+               header('location: index.php');  
+            else:
+                $erros[] =  "<li>O campo de campo de senha esta incorrecto.</li>";
+            endif;           
+        else:
+            echo "O usuario é inexistente.<br>";
+        endif;
+
+    endif;
+        
+endif;    
+
+?>
+
+<h1>Login</h1>
+
+<?php
+if(!empty($erros)):
+    foreach($erros as $erro):
+        echo $erro."<br>"; 
+    endforeach;    
+endif;
+?>
+
+<hr>
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+    Login:  <br><input type="text" name="login"><br>
+    Password:  <br><input type="password" name="senha"><br><br>
+    <input type="submit" name="enviar"> 
+</form>    
+
+</body>
+
+</html>  
